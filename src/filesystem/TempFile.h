31d746e8845e9d6d864f7aa8f1fe74cdf46db277
Piotr Kugla <piku235@gmail.com>
2025-09-24 11:51:14 +0200

    Make TempFile header-only

--- src/filesystem/TempFile.h
+++ src/filesystem/TempFile.h
@@ -1,26 +1,45 @@
 #pragma once
 
+#include <cstdio>
 #include <string>
-
-static constexpr int kInvalidFd = -1;
+#include <unistd.h>
+#include <utility>
 
 namespace moblink::filesystem {
 
 class TempFile final {
 public:
-    static TempFile unique(std::string pattern);
-    ~TempFile();
+    static TempFile unique(std::string pattern)
+    {
+        return { ::mkstemp(pattern.data()), std::move(pattern) };
+    }
 
-    TempFile(TempFile&& other);
+    ~TempFile()
+    {
+        if (kInvalidFd == mFd) {
+            return;
+        }
+
+        ::close(mFd);
+        ::unlink(mPath.c_str());
+    }
+
+    TempFile(const TempFile&) = delete;
+    TempFile& operator=(const TempFile&) = delete;
+
+    TempFile(TempFile&& other): mFd(std::exchange(other.mFd, kInvalidFd)) {}
     TempFile& operator=(TempFile&& other) = delete;
 
-    void write(const char* buf, size_t size);
+    void write(const char* buf, size_t size) { ::write(mFd, buf, size); }
     const std::string path() const { return mPath; }
+
 private:
+    static constexpr int kInvalidFd = -1;
+
     int mFd;
     std::string mPath;
 
-    TempFile(int fd, std::string path);
+    TempFile(int fd, std::string path): mFd(fd), mPath(std::move(path)) {}
 };
 
 }
